c086bbd939fe337579e985c4e9965db1e612ed0d
PearlinIda <pearlinida@gmail.com>
2024-03-25 12:54:43 +0530

    Initial Code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { 
+      path: 'dashboard', 
+      loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) 
+    },
+// Other routes
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
